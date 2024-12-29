76d1c7ebfc923700791c8705372415386c686b3b
Moibe <moi_estrello@hotmail.com>
2024-12-29 02:37:41 -0600

    Add Quota Type

--- globales.py
+++ globales.py
@@ -1,12 +1,20 @@
 import gradio as gr
 
 #MAIN
-version = "4.2.6"
+version = "5.3.6"
 env = "prod"
 aplicacion = "astroblend"
-api_zero = "Moibe/image-blend"
-api_cost = "Moibe/image-blend"
-same_api = True
+seleccion_api = "eligeAOB" #eligeGratisOCosto , eligeAOB o eligeGratisOCosto
+max_size = 20
+#Quota o Costo
+api_zero = ("Moibe/image-blend", "quota")
+api_cost = ("Moibe/image-blend", "costo")
+#A o B
+api_a = ("Moibe/image-blend", "gratis")
+api_b = ("Moibe/image-blend", "gratis")
+#Gratis o Costo
+api_gratis = ("Moibe/image-blend", "gratis")
+api_costo = ("Moibe/image-blend", "costo")
 process_cost = 0
 seto = "image-blend"
 work = "picswap"
